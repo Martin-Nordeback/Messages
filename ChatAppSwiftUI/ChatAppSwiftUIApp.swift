148b84fab499f560d401290c132400f9d23f3d74
Martin-Nordeback <martin.nordeback@gmail.com>
2023-04-12 11:46:12 +0200

    Initial Commit

---
+++ ChatAppSwiftUI/ChatAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSwiftUIApp.swift
+//  ChatAppSwiftUI
+//
+//  Created by Martin Nordebäck on 2023-04-12.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
